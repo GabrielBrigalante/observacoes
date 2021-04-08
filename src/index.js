80e9f34a14888f5616fce6d2b454ce509e25eaa4
GabrielBrigalante <79880931+GabrielBrigalante@users.noreply.github.com>
2021-04-08 19:14:35 -0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -5,8 +5,13 @@ app.use(express.json());
 //exemplo: /lembretes/123456/observacoes
 app.put('/lembretes/:id/observacoes', (req, res) => {
 });
+
+
 app.get('/lembretes/:id/observacoes', (req, res) => {
 });
+
+
+
 app.listen(5000, (() => {
 console.log('Lembretes. Porta 5000');
 }));
